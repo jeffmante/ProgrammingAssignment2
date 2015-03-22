a494efeafbc606ee02ed17d41ffbd5e76713182c
jeffman <jeffmante@users.noreply.github.com>
2015-03-22 21:12:17 +0800

    Update cachematrix.R
    Updated this file with the code to invert the matrix and with the comments for its description.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## caching the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+#1.  set the value of the matrix
+#2.  get the value of the matrix
+#3.  set the value of the inverse
+#4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        j <- NULL
+        set <- function(y) {
+                x <<- y
+                j <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) j <<- solve
+        getinv <- function() j
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+        
 }
 
 
-## Write a short comment describing this function
+## `cacheSolve` function computes the inverse of the special "matrix" returned by `makeCacheMatrix` above. 
+#If the inverse has already been calculated (and the matrix has not changed), then`cacheSolve` should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## At the moment giving: Error in x$getinv : $ operator is invalid for atomic vectors
+cachesolve <- function(x, ...) { 
+        j<- x$getinv()
+        if(!is.null(j)) {
+                message("getting cached data")
+                return(j)
+        }
+        data <- x$get()
+        j<- solve(data, ...)
+        x$setinv(j)
+        j
+        
 }
